6cd0dc2a40b1f7bd35a92f57aebb5e7b94265332
PJScript <pjs771188@gmail.com>
2024-01-10 17:41:59 +0900

    feat(article): 게시글 수정 시 비밀번호 검증 페이지 구현

--- src/main/java/com/example/communitydemo/domain/board/controller/ArticleController.java
+++ src/main/java/com/example/communitydemo/domain/board/controller/ArticleController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -51,24 +52,70 @@ public class ArticleController {
         return String.format("redirect:/article/%d", newId);
 
     }
-    // 게시글 수정 view
+
+    /**
+
+     * 게시글 수정 view
+     */
     @GetMapping("{id}/modify")
-    public String articleModifyView(Model model, @PathVariable Long id){
-        model.addAttribute("article",articleService.articleRead(id));
-        return "articleModify";
+    public String articleModifyView(
+            Model model,
+            @PathVariable Long id,
+            @RequestParam("token") String token
+    ){
+        if(token == null){
+            return "accessDenied";
+        }
+        boolean isPossible = Long.parseLong(token) == id;
+
+        if(isPossible){
+            model.addAttribute("article",articleService.articleRead(id));
+            return "articleModify";
+        }else{
+            return "accessDenied";
+        }
     }
 
 
+    /**
+     * 게시글 수정 전 비밀번호 검증 페이지
+     * @return
+     */
+    @GetMapping("{id}/modify/password-check")
+    public String articleModifyPasswordCheckView(
+            Model model,
+            @PathVariable("id") Long id){
+        model.addAttribute("articleId",id);
+        return "articleModifyPasswordCheck";
+    }
 
 
+    /**
+     * 게시글 수정 페이지 접근 가능 여부 확인 후 접근 가능하면 리다이렉트
+     */
+    @PostMapping("{id}/modify/password-check")
+    public String articleModifyPasswordCheck(
+            @PathVariable Long id,
+            @ModelAttribute ArticleDto.ArticlePasswordCheckRequest request
+          ) {
+        boolean isPossible = articleService.articlePasswordCheck(id, request);
 
-    // 게시글 수정 가능 여부 체크 ( 비밀번호 )
-    @PostMapping("{id}/password-check")
-    public Boolean articlePasswordCheck(
+        System.out.printf("요청 체크");
+        if (isPossible) {
+            Long token = id; // 일회용 토큰 생성
+            return "redirect:/article/" + id + "/modify?token=" + token;
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @PostMapping("{id}/delete/password-check")
+    public void articleDeletePasswordCheck(
             @PathVariable Long id,
             @RequestBody ArticleDto.ArticlePasswordCheckRequest request
     ) {
-        return articleService.articlePasswordCheck(id,request);
+//        return articleService.articlePasswordCheck(id,request);
+
     }
 
     // 게시글 수정
